3584806cb048e55f4d23b1c9a80b5848cb37513e
unknown <sunilgautam2060@gmail.com>
2026-08-31 19:47:08 +0545

    Cyclic Sort Headache

--- PythonBasics/DSA/Cyclic Sort/FindDuplicateNumber.py
+++ PythonBasics/DSA/Cyclic Sort/FindDuplicateNumber.py
@@ -9,33 +9,37 @@
 # 9 -> index 8
 
 
+#cyclic sort preassumed or fixed defined the correct position of the number like:
+#1 position is 0th index , 5 position is 4th for sure its like fixed or something like that
+#in so following that algorithm the array element gets placed . 
+
 List = [1,3,2,7,4,6,5,2,7,8,9,3]
 
-i = 0
+current_index = 0
 DuplicateNumber = []
 
-while i < len(List):
+while current_index < len(List):
 
-    correct_index = List[i] - 1
+    correct_index = List[current_index] - 1
 
     # If the value is already present at its correct index,
     # then the current value is a duplicate.
 
-    if List[i] == List[correct_index]:
+    if List[current_index] == List[correct_index]:
 
-        if i != correct_index:
-            DuplicateNumber.append(List[i])
+        if current_index != correct_index:
+            DuplicateNumber.append(List[current_index])
 
-        i = i + 1
+        current_index = current_index + 1
 
     else:
 
         # Put the value into its correct position.
 
-        temp = List[i]
-        List[i] = List[correct_index]
+        temp = List[current_index]
+        List[current_index] = List[correct_index]
         List[correct_index] = temp
-
+ 
 
 print("Sorted List :", List)
 print("Duplicate Numbers :", DuplicateNumber)
